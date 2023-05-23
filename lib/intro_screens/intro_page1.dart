ae83ba5de4bf91d496b470c732f3324af746ec5b
kuldeep23 <kuldeepnitian@gmail.com>
2023-05-23 18:40:46 +0530

    Initial commit

---
+++ lib/intro_screens/intro_page1.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class IntroPage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.pink[100],
+      child: Center(
+        child: Text('Intro Page 1'),
+      ),
+    );
+  }
+}
---
+++ lib/intro_screens/intro_page2.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class IntroPage2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purple[100],
+      child: Center(
+        child: Text('Intro Page 2'),
+      ),
+    );
+  }
+}
